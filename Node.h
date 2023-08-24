96f4584cf0ca9d0b95c235e6f8185d6d40a326d1
mariammahrous23 <71783484+mariammahrous23@users.noreply.github.com>
2023-08-24 20:33:52 +0300

    test

--- Node.h
+++ Node.h
@@ -3,7 +3,7 @@
 #include <cmath>
 
 class Node {
-Private:
+private:
     int x;          // X (row) coordinate of the node on the grid
     int y;          // Y (col) coordinate of the node on the grid
     bool walkable;  // Whether the node is an walkable or not
@@ -15,7 +15,7 @@ Private:
 
     Node* parent;   // Parent node in the path
 
-Public:
+public:
     // Constructor
     Node(int x, int y, bool obstacle = false)
         : x(x), y(y), walkable(walkable), gCost(0), hCost(0), fCost(0), parent(nullptr) {
@@ -27,27 +27,27 @@ Public:
     }
 
     //Get parent 
-     Node* GetParent() {
-        return parent ;
+    Node* getParent() {
+        return parent;
     }
 
-    // Calculate gcost in manhttan way but prefer diagonal movements
+    // Calculate gcost 
     double calculateGCost(Node* startNode) {
-    int row = std::abs(x - startNode->x);
-    int col = std::abs(y - startNode->y);
-    gCost = row + col; 
-    return gCost;
-    }   
-
-    // Calculate Hcost in manhttan way but prefer diagonal movements
-   double calculateHCost(Node* goalNode) {
-    int row = std::abs(x - goalNode->x);
-    int col = std::abs(y - goalNode->y);
-    hCost = row + col ;
-    return hCost;
+        int row = std::abs(x - startNode->x);
+        int col = std::abs(y - startNode->y);
+        gCost = std::sqrt((row * row) + (col * col));
+        return gCost;
     }
 
-    double calculateFCost( Node* startNode , Node* goalNode ) {
+    // Calculate Hcost 
+    double calculateHCost(Node* goalNode) {
+        int row = std::abs(x - goalNode->x);
+        int col = std::abs(y - goalNode->y);
+        hCost = std::sqrt((row*row) +(col*col));
+        return hCost;
+    }
+
+    double calculateFCost(Node* startNode, Node* goalNode) {
         fCost = calculateGCost(startNode) + calculateHCost(goalNode);
         return fCost;
     }
