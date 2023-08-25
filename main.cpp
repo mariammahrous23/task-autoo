31622675adda7859b4cbe0d6105ccb9520173e03
Amira764 <130529827+Amira764@users.noreply.github.com>
2023-08-25 14:04:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,17 +12,168 @@ main.exe warehouse1.txt
 */
 
 #include "warehouse.h"
+#include "node.h"
+#include"stack.h"
 
-void initialize()
+void initialize() //return nodemap
 {
+    Node nodemap[__privates::mapSize][__privates::mapSize]; 
+   for (int row = 0; row < __privates::mapSize; row++)
+    {
+        for (int col = 0; col < __privates::mapSize; col++)
+        {
+            bool iswalakable = __privates::map[row][col];
+            Node temp(row,col,iswalakable);
+            nodemap[row][col]=temp;
+        }            
+    }
+
+    //grid -> nodes in 2D array or map grid
     /*
     This function will run after the warehouse is loaded.
     */
-    
 }
 
-void solve()
+Node* getminfcost(Node *open[], int size) //needs more validation
 {
+    Node* temp = nullptr;
+    int min = 100000000000000; //dummy very large number, should be inf or 1st element
+    for (int i=0; i<size; i++)
+    {
+        if (open[i]->fCost!=-1 && open[i]->fCost < min)
+        {
+            min = open[i]->fCost;
+            temp = open[i];
+        }
+    }
+    return temp;
+}
+
+void removeFromOpen(Node *open[],Node * current, int &size)
+{
+    for(int i=0; i<size; i++)
+    {
+        if (open[i]->x==current->x && open[i]->y==current->y) //ashyak b operator overload
+        {
+            open[i]=open[size-1];
+            open[size-1]=nullptr;
+            size--;
+            break;
+        }
+    }
+}
+
+bool isInClosed(Node* closed[], Node* neighbour, int size)
+{
+    for(int i=0; i<size; i++)
+    {
+        if (closed[i]->x == neighbour->x && closed[i]->y == neighbour->y)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isInOpen(Node* open[], Node* neighbour, int size)
+{
+    for(int i=0; i<size; i++)
+    {
+        if (open[i]->x == neighbour->x && open[i]->y == neighbour->y)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void generatepath(Stack<Node*> path, Node*start)
+{
+    if(start)
+    {
+        path.push(start);
+        generatepath(path,start->parent);
+    }
+    else {return;}
+}
+
+
+void solve() //nestelem el nodemap
+{
+/////////////////
+   Node nodemap[__privates::mapSize][__privates::mapSize]; 
+   for (int row = 0; row < __privates::mapSize; row++)
+    {
+        for (int col = 0; col < __privates::mapSize; col++)
+        {
+            bool iswalakable = __privates::map[row][col];
+            Node temp(row,col,iswalakable);
+            nodemap[row][col]=temp;
+        }            
+    }
+  
+   Node start (getRobotPos().col,getRobotPos().row,true);
+   Node* startptr = &start;
+   Node pickup(getItemPos().col,getItemPos().row,true);
+   Node * pickptr = &pickup;
+/////////////////
+    //open list
+    //closed list
+    //hanerga3 bel parent n.save el path by pushing b recursion stack
+    //pop to move robot
+    //calc relative position
+    //give params to move robot
+
+    Node * closed[121];
+    int closedsize =0;
+    Node * current = &nodemap[__privates::robotPos.row][__privates::robotPos.col];
+    Node * open[121]; //max size, can be better optimised if linkedlist
+    open[0]=current;
+    current->calculateFCost(startptr,pickptr);
+    int opensize=1;
+    bool picked = (current->x ==__privates::pickupPos.col) && (current->y= __privates::pickupPos.row);
+    //Node * MinFcostNode = nullptr; //for comparison with neighbours
+    while(!picked)
+    {
+        current = getminfcost(open, opensize);
+        removeFromOpen(open,current,opensize);
+        closed[closedsize] = current;
+        closedsize++;
+        //traversing the 8 neighbouring cells of the 'current'
+        for(int i= current->x-1; i<=current->x+1; i++)
+        {
+            for(int j=current->y-1; j<=current->y+1; j++)
+            {
+                Node neighbour = nodemap[i][j];
+                if(!neighbour.walkable || isInClosed(closed,&neighbour,closedsize))
+                {continue;}
+                if(!isInOpen(open,&neighbour,opensize) || neighbour.calculateFCost(startptr,pickptr)<getminfcost(open, opensize)->calculateFCost(startptr,pickptr))
+                {
+                    neighbour.parent= current;
+                    open[opensize]=&neighbour;
+                }
+            }
+        }
+        picked = (current->x ==__privates::pickupPos.col) && (current->y= __privates::pickupPos.row);
+    }
+    //by the end of this loop, robot has picked up the item and the current = pickup pos
+    //now we recurse back with parents and put the nodes in a stack
+    Stack<Node*> path;
+    generatepath(path,current);
+    //now that the stack contains or nodes to the path
+    //time to move robot by popping the nodes, calculating relative pos, giving it to the robot
+    //pop , calc rel pos, move robot, get next, do again
+    Node* nextmove = path.pop()->data;
+    while (nextmove)
+    {
+        int movex = nextmove->x - getRobotPos().col;
+        int movey = nextmove->y - getRobotPos().row;
+        moveRobot(movex,movey);
+        nextmove = path.pop()->data;
+    }
+    //by end of this while loop, robot would have reached the pickup pos
+    //same algorithm is to be repeated for the pickup to destination part
+
     /*
     This function will keep running forever until the goal is reached.
 
@@ -45,18 +196,12 @@ void solve()
 
 int main(int argc, char const *argv[])
 {
+
     // Initalize level
     initMaze(argv[1]);
-
+    
     initialize();
-    cout << endl;
-    cout << " mimo is here ";
-    cout << endl;
-    cout <<__privates::robotPos.row;
-    cout << endl;
-    printMaze();
-    cout << endl;
-    cout << " mimo is here " << endl;
+
     // Game loop
     while (true)
     {
@@ -69,8 +214,8 @@ int main(int argc, char const *argv[])
         }
 
         // Solve maze
-        solve();
+        solve(); 
     }
 
-    return 0;
+    return 0; 
 }
